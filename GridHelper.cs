2a098c00763f77c87f0a156d42bea1c73d7b9c74
jgreen12 <jgreen12@islander.tamucc.edu>
2016-03-28 10:23:51 -0500

    Added files via upload

---
+++ GridHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridHelper : MonoBehaviour {
+    public int radius;
+
+	// Use this for initialization
+	void Start () {
+        radius = 3;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ HexGenOrigin.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexGenOrigin : MonoBehaviour {
+
+    public GameObject Hex;
+    
+
+    public float hexWidth;
+    public float hexHeight;
+    public int numRows;
+
+    Vector3 Origin;
+
+    private bool found;
+
+    private double diagMag;
+
+
+    // Use this for initialization
+
+    void setVals()
+    {
+        
+        
+    }
+
+    void budder(int diameter)
+    {
+        Origin = new Vector3(0, 0, 0);
+        numRows = diameter / 2;
+        int count = 0;
+        if (diameter != 0)
+        {
+            //check left
+            //Left create
+            GameObject hex = (GameObject)Instantiate(Hex);
+            hex.transform.position = Origin;
+            hex.GetComponent<HexMapGen>().budder(diameter, numRows, count);
+
+            //instantiate a new object
+            //set transform to 
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        
+        budder(31);
+	}
+	
+
+}
